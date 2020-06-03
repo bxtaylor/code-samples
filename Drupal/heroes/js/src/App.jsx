10a7de82e5ead8e2715c64ba369ca47a5f1536c3
Bobby Taylor <bobby@boomchuck.dev>
2020-06-02 23:22:39 -0600

    ADD: heroes react-js module.

---
+++ Drupal/heroes/js/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import HeroesList from "./Components/HeroesList";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Heroes!!!</h1>
+        <HeroesList />
+      </>
+    )
+  }
+}
+
+export default App;
---
+++ Drupal/heroes/js/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// # Example 1: Simple "Hello, World" code
+ReactDOM.render(
+  <App />,
+  document.getElementById('heroes-app')
+);
